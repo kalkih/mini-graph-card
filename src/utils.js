900cd15a6018a34871a0bae3fe60ff02dd009db0
Bram Kragten <mail@bramkragten.nl>
2019-05-24 14:51:03 +0200

    lint

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-bitwise */
 import { compress as lzStringCompress, decompress as lzStringDeompress } from './lz-string';
 
-const getMin = (arr, val) => arr.reduce((min, p) => (Number(p[val]) < Number(min[val]) ? p : min), arr[0]);
-const getMax = (arr, val) => arr.reduce((max, p) => (Number(p[val]) > Number(max[val]) ? p : max), arr[0]);
+const getMin = (arr, val) => arr.reduce((min, p) => (
+  Number(p[val]) < Number(min[val]) ? p : min
+), arr[0]);
+const getMax = (arr, val) => arr.reduce((max, p) => (
+  Number(p[val]) > Number(max[val]) ? p : max
+), arr[0]);
 const getTime = (date, extra, locale = 'en-US') => date.toLocaleString(locale, { hour: 'numeric', minute: 'numeric', ...extra });
 const getMilli = hours => hours * 60 ** 2 * 10 ** 3;
 
