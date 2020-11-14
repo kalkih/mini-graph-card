15c76695bfde832e815a39c046e1c448bffe4f5f
dcs8 <cookscowen@gmail.com>
2020-11-14 22:54:36 +0000

    Make card appear in lovelace card picker

--- src/main.js
+++ src/main.js
@@ -916,3 +916,12 @@ class MiniGraphCard extends LitElement {
 }
 
 customElements.define('mini-graph-card', MiniGraphCard);
+
+// Configure the preview in the Lovelace card picker
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: 'mini-graph-card',
+  name: 'Mini Graph Card',
+  preview: false,
+  description: 'The Mini Graph card is a minimalistic and customizable graph card',
+});
