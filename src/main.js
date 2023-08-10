6bbb3609abc318556ab733b434cc1284ddf9f082
Eric Vandecasteele <eric@onlinux.fr>
2023-08-10 15:18:54 +0200

    remove if condition

--- src/main.js
+++ src/main.js
@@ -910,13 +910,8 @@ class MiniGraphCard extends LitElement {
       if (this.config.state_map.length > 0 || this.config.entities[index].attribute) {
         newStateHistory[0].forEach((item) => {
           if (this.config.entities[index].attribute) {
-            if (this.config.entities[index].attribute.includes('.')) {
-              // eslint-disable-next-line no-param-reassign
               item.state = this.getObjectAttr(item.attributes,
                 this.config.entities[index].attribute);
-            } else
-              // eslint-disable-next-line no-param-reassign
-              item.state = item.attributes[this.config.entities[index].attribute];
             // eslint-disable-next-line no-param-reassign
             delete item.attributes;
           }
