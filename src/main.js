3e398e47a9e1349e07737aa6586ac84131d58783
Julien Deveaux <julien.deveaux@hotmail.com>
2024-12-12 15:12:01 +0100

    style: better looking loader

--- src/main.js
+++ src/main.js
@@ -327,7 +327,7 @@ class MiniGraphCard extends LitElement {
     return svg`
         <div style="align-items: center; justify-content: center; display: flex;">
             <svg width="100%" height="100%" viewBox="0 0 500 ${height}">
-                <circle cx="50%" cy="50%" r="0">
+                <circle cx="50%" cy="50%" r="0" fill="#818181">
                     <animate attributeName="r" calcMode="spline" dur="1.2s" values="10;20" keySplines=".52,.6,.25,.99" repeatCount="indefinite"/>
                     <animate attributeName="opacity" calcMode="spline" dur="1.2s" values="1;0" keySplines=".52,.6,.25,.99" repeatCount="indefinite"/>
                 </circle>
