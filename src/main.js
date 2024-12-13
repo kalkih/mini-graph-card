a95079618d4b8b4559776be0fbd3c7546051f4bb
Julien Deveaux <julien.deveaux@hotmail.com>
2024-12-13 11:22:39 +0100

    style: better grid lines customizability

--- src/main.js
+++ src/main.js
@@ -563,9 +563,9 @@ class MiniGraphCard extends LitElement {
     for (let i = 0; i < numLines; i += 1) {
       const x = xRatio * (i + 0.5);
       if (i % grid_lines_ratio > 0) {
-        lines.push(svg`<line x1=${x} y1="0" x2=${x} y2=${height} stroke="var(--divider-color)" stroke-width="0.5"/>`);
+        lines.push(svg`<line x1=${x} y1="0" x2=${x} y2=${height} stroke="var(--mini-graph-card-thin-grid-color, var(--divider-color))" stroke-width="0.5"/>`);
       } else {
-        lines.push(svg`<line x1=${x} y1="0" x2=${x} y2=${height} stroke="rgb(from var(--divider-color) R G B /0.5)" stroke-width="0.5"/>`);
+        lines.push(svg`<line x1=${x} y1="0" x2=${x} y2=${height} stroke="var(--mini-graph-card-thick-grid-color, rgb(from var(--divider-color) R G B /0.5))" stroke-width="0.5"/>`);
       }
     }
 
