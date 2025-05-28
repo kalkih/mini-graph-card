41d737cbd3dfb5fa83493d6ae24876a0b149e8ef
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-05-28 23:10:03 +0300

    added isPre2025_5

--- src/utils.js
+++ src/utils.js
@@ -27,8 +27,11 @@ const log = (message) => {
   console.warn('mini-graph-card: ', message);
 };
 
+const isPre2025_5 = (hass) => hass && hass.config.version.split(".")[0] === "2025" && Number(hass.config.version.split(".")[1]) < 5;
+
 export {
   getMin, getAvg, getMax, getTime, getMilli, compress, decompress, log,
   getFirstDefinedItem,
   compareArray,
+  isPre2025_5,
 };
