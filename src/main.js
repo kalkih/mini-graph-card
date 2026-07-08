d98fd8af2ee41e0f7ec024940783771a0156167e
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-07-08 05:52:03 +0300

    ass static values labels

--- src/main.js
+++ src/main.js
@@ -20,7 +20,9 @@ import {
   UPDATE_PROPS,
   X, Y, V,
   ONE_HOUR,
+  DEFAULT_GRAPH_HEIGHT,
   DEFAULT_MARGIN,
+  DEFAULT_STATIC_VALUE_LABEL_OFFSET,
 } from './const';
 import {
   getFactor,
@@ -62,6 +64,7 @@ class MiniGraphCard extends LitElement {
     this.stateChanged = false;
     this.initial = true;
     this._md5Config = undefined;
+    this.staticValueLabelOffset = undefined; // offset (x) for labels for static lines
 
     // update datetime settings periodically
     this.updateHour24 = true;
@@ -154,6 +157,9 @@ class MiniGraphCard extends LitElement {
     this.config = buildConfig(config);
     this._md5Config = SparkMD5.hash(JSON.stringify(this.config));
     const entitiesChanged = !compareArray(this.config.entities || [], config.entities);
+
+    // set offset (x) for labels for static lines
+    this.prepareStaticValueLabelOffset();
 
     // update datetime settings periodically
     this.updateHour24 = config.hour24 === undefined;
@@ -256,6 +262,8 @@ class MiniGraphCard extends LitElement {
   }
 
   updated(changedProperties) {
+    super.updated(changedProperties);
+
     if (this.config.animate && changedProperties.has('line')) {
       if (this.length.length < this.entity.length) {
         this.shadowRoot.querySelectorAll('svg path.line').forEach((ele) => {
@@ -502,26 +510,38 @@ class MiniGraphCard extends LitElement {
     `;
   }
 
+  /**
+  * Renders a Graph element (along with a legend)
+  * @returns {TemplateResult} Lit template result
+  */
   renderGraph() {
-    const ready = ((this.entity[0] || this.isStaticValue(0))
+    const hasInitialData = this.entity
+      && (this.entity[0] || this.isStaticValue(0));
+    const ready = (hasInitialData
       && !this.Graph.some(
         (element, index) => element._history === undefined
           && this.config.entities[index].show_graph !== false,
       ))
     || this.config.show.loading_indicator === false;
-    return this.config.show.graph ? html`
-      <div class="graph">
-        ${ready ? html`
-            <div class="graph__container">
-              ${this.renderLabels()}
-              ${this.renderLabelsSecondary()}
-              <div class="graph__container__svg">
-                ${this.renderSvg()}
-              </div>
-            </div>
-            ${this.renderLegend()}
-        ` : html`<ha-spinner aria-label="Loading" size="small"></ha-spinner>`}
-      </div>` : '';
+
+    return this.config.show.graph
+      ? html`
+          <div class="graph">
+            ${ready
+              ? html`
+                  <div class="graph__container">
+                    ${this.renderLabels()}
+                    ${this.renderLabelsSecondary()}
+                    <div class="graph__container__svg">
+                      ${this.renderSvg()}
+                      ${this.renderStaticLabels()}
+                    </div>
+                  </div>
+                  ${this.renderLegend()}
+                `
+              : html`<ha-spinner aria-label="Loading" size="small"></ha-spinner>`}
+          </div>`
+      : html``;
   }
 
   /**
@@ -578,6 +598,76 @@ class MiniGraphCard extends LitElement {
       <svg width='10' height='10'>
         <rect width='10' height='10' fill=${this.computeColor(state, index)} />
       </svg>
+    `;
+  }
+
+  prepareStaticValueLabelOffset() {
+    const { static_value_label_offset: offset } = this.config;
+    this.staticValueLabelOffset = DEFAULT_STATIC_VALUE_LABEL_OFFSET;
+
+    if (offset === undefined || offset === null) return;
+    if (isNumeric(offset)) {
+      this.staticValueLabelOffset = Number(offset);
+    } else {
+      log('value of static_value_label_offset is incorrect, a default value is used');
+    }
+  }
+
+  /**
+  * Renders static lines' labels
+  * @returns {TemplateResult} Lit template result
+  */
+  renderStaticLabels() {
+    if (!this.config.show.static_value_labels) {
+      return html``;
+    }
+
+    const graphHeight = this.config.height !== undefined
+      ? this.config.height : DEFAULT_GRAPH_HEIGHT;
+    if (!isNumeric(graphHeight) || graphHeight <= 0) {
+      return html``;
+    }
+
+    const isLeft = this.config.show.static_value_labels === 'left';
+
+    return html`
+      <div class="graph__static_value_labels" loc="${this.config.show.static_value_labels}">
+        ${this.config.entities.map((_, index) => {
+          if (!this.isStaticValue(index) || this.config.entities[index].show_static_value_label === false) {
+            return;
+          }
+          const staticValue = this.config.entities[index].static_value;
+          // get Y coord in SVG space
+          const [staticLineCoord] = this.Graph[index]._calcY([[0, 0, staticValue]]);
+          const [, topSVG] = staticLineCoord; // top in SVG coords
+
+          const topPercent = (topSVG / graphHeight) * 100; // top in %
+          if (!isNumeric(topPercent)) {
+            return;
+          }
+
+          const offset = this.staticValueLabelOffset; // offset in %
+
+          const color = this.config.entities[index].state_adaptive_color
+            ? this.computeColor(staticValue, index)
+            : 'var(--primary-text-color)';
+
+          return html`
+            <span
+              id="static-label-${index}"
+              ?inactive=${this.tooltip.entity !== undefined && this.tooltip.entity !== index
+                && !this.isShowStaticInactive(index)}
+              style="
+                color: ${color};
+                top: ${topPercent}%;
+                left: ${isLeft ? `${offset}%` : `calc(100% - ${offset}%)`};
+              "
+            >
+              ${this.computeState(staticValue, index)}
+            </span>
+          `;
+        })}
+      </div>
     `;
   }
 
@@ -768,7 +858,7 @@ class MiniGraphCard extends LitElement {
   }
 
   /** Returns all rendered SVG parts (fill, line, bars, points)
-  * @returns {any} SVG
+  * @returns {any} SVG element
   */
   renderSvg() {
     const { height, show } = this.config;
