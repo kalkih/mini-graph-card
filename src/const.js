92ecdbbfce99aa7a57e93dc2a37022316961a46f
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-07-08 03:04:26 +0300

    add DEFAULT_BAR_SPACING

--- src/const.js
+++ src/const.js
@@ -3,6 +3,7 @@ const FONT_SIZE = 14;
 const FONT_SIZE_HEADER = 14;
 const MAX_BARS = 96;
 const DEFAULT_MARGIN = 5;
+const DEFAULT_BAR_SPACING = 4;
 const ICONS = {
   humidity: 'hass:water-percent',
   illuminance: 'hass:brightness-5',
@@ -57,6 +58,7 @@ export {
   FONT_SIZE_HEADER,
   MAX_BARS,
   DEFAULT_MARGIN,
+  DEFAULT_BAR_SPACING,
   ICONS,
   DEFAULT_COLORS,
   UPDATE_PROPS,
