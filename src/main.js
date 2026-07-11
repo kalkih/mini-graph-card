0175f05b190d84d6bf8283b63eb8f2d0ae008025
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-07-11 03:50:32 +0300

    add cached array for visibleBarEntities

--- src/main.js
+++ src/main.js
@@ -88,6 +88,7 @@ class MiniGraphCard extends LitElement {
 
     // initailize memoized arrays
     this._visibleEntitiesCache = null;
+    this._visibleBarEntitiesCache = null;
     this._primaryYaxisEntitiesCache = null;
     this._secondaryYaxisEntitiesCache = null;
     this._visibleLegendsCache = null;
@@ -1018,6 +1019,15 @@ class MiniGraphCard extends LitElement {
         .filter(entity => entity.show_graph !== false);
     }
     return this._visibleEntitiesCache;
+  }
+
+  get visibleBarEntities() {
+    if (!this._visibleBarEntitiesCache) {
+      this._visibleBarEntitiesCache = this.config.show.graph === 'bar'
+        ? this.visibleEntities
+        : this.visibleEntities.filter(entity => entity.graph === 'bar');
+    }
+    return this._visibleBarEntitiesCache;
   }
 
   get primaryYaxisEntities() {
