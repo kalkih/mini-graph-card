55fa1ab78ab35a7e11b2b958b3ca3d94acfccc19
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-07-23 23:48:11 +0300

    add bars-related options into constructor

--- src/graph.js
+++ src/graph.js
@@ -2,7 +2,9 @@ import { interpolateRGB } from './color';
 import {
   X, Y, V,
   ONE_HOUR,
+  DEFAULT_BAR_SPACING,
 } from './const';
+import { log } from './utils';
 
 export default class Graph {
   constructor({
@@ -15,6 +17,9 @@ export default class Graph {
     groupBy = 'interval',
     smoothing = true,
     logarithmic = false,
+    bar_spacing = DEFAULT_BAR_SPACING, // spacing between bars
+    bar_spacing_group = DEFAULT_BAR_SPACING, // spacing between groups of bars
+    total_bars_in_group = 1, // number of bars (i.e. number of entities with a shown bar graph)
   }) {
     const aggregateFuncMap = {
       avg: this._average,
@@ -41,6 +46,9 @@ export default class Graph {
     this._calcPoint = aggregateFuncMap[aggregateFuncName] || this._average;
     this._smoothing = smoothing;
     this._logarithmic = logarithmic;
+    this.bar_spacing = bar_spacing;
+    this.bar_spacing_group = bar_spacing_group;
+    this.total_bars_in_group = total_bars_in_group;
     this._groupBy = groupBy;
     this._endTime = 0;
   }
@@ -211,22 +219,34 @@ export default class Graph {
    * Get bars for an entity
    * @param {number} position Index of a bar (0,1,..)
    * (i.e. index of an entity with a shown bar graph)
-   * @param {number} total Number of bars (i.e. number of entities with a shown bar graph)
-   * @param {number} spacing Spacing between bars
-   * @param {number} spacing_group Spacing between groups of bars
    * @returns Bars for an entity to be shown at a `position` index
    */
-  getBars(position, total, spacing, spacing_group) {
+  getBars(position) {
+    const spacing = this.bar_spacing;
+    const spacing_group = this.bar_spacing_group;
+    const total = this.total_bars_in_group;
+
     const coords = this._calcY(this.coords);
+
     // number of measures
     const total_groups = Math.ceil(this.hours * this.points);
+
     // width of a group of bars
     const group_width = (this.width - spacing_group * (total_groups - 1))
       / total_groups;
-    // a width of a bar
-    const bar_width = spacing === -1
-      ? group_width
-      : (group_width - spacing * (total - 1)) / total;
+
+    // width of a bar
+    let bar_width;
+    if (spacing === -1) {
+      bar_width = group_width;
+    } else {
+      bar_width = (group_width - spacing * (total - 1)) / total;
+      if (bar_width <= 0) {
+        bar_width = 1;
+        log(`Invalid bar_width, adjusted to 1 (bar_spacing ${spacing}, bar_spacing_group ${spacing_group})`);
+      }
+    }
+
     return coords.map((coord, i) => ({
       x: this.margin[X]
         + (group_width + spacing_group) * i
