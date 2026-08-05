60cb641769cbccacf7fbcdce75fdf1cb5d842519
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-08-06 02:36:45 +0300

    feat: Add tests for checkNumericOption(), checkIntegerOption() functions (#1417)
    * Create checkNumericOption.test.ts.dis
    
    * Create checkOption.js
    
    * move checkNumericOption & checkIntegerOption into a separate module
    
    * remove unneeded import

--- src/buildConfig.js
+++ src/buildConfig.js
@@ -12,82 +12,8 @@ import {
   DEFAULT_COLORS,
   DEFAULT_SHOW,
 } from './const';
-import { isNumeric } from './others';
 import { log } from './utils';
-
-/**
- * Check if an option is numeric (if not undefined);
- * fallback to a default value if not numeric or out of bounds
- * @param {object} config Config object
- * @param {string} option Name of option to be checked
- * @param {number} defaultValue Default fallback value
- * @param {number} minBound Optional minimum allowed value
- * @param {number} maxBound Optional maximum allowed value
- * @returns {number} Cleared value
- */
-const checkNumericOption = (
-  config,
-  option,
-  defaultValue,
-  minBound = undefined,
-  maxBound = undefined,
-) => {
-  const value = config[option];
-
-  if (value === undefined || value === null) {
-    return undefined;
-  }
-
-  if (isNumeric(value)) {
-    const isMinValid = minBound === undefined || value >= minBound;
-    const isMaxValid = maxBound === undefined || value <= maxBound;
-    if (isMinValid && isMaxValid) {
-      return value;
-    }
-  }
-
-  const clearedValue = defaultValue;
-  const invalidValue = typeof value === 'object'
-    ? JSON.stringify(value)
-    : value;
-  let errorDescr = 'not a numeric value';
-  if (isNumeric(value)) {
-    if (minBound !== undefined && value < minBound) {
-      errorDescr = `out of bounds, minimum allowed: ${minBound}`;
-    } else if (maxBound !== undefined && value > maxBound) {
-      errorDescr = `out of bounds, maximum allowed: ${maxBound}`;
-    }
-  }
-  log(`Invalid option ${option}: [${invalidValue}] (${errorDescr}); adjusting value to ${clearedValue}`);
-  return clearedValue;
-};
-
-/**
- * Check if an option is integer;
- * fallback to a default value if not numeric or out of bounds;
- * round to an integer if needed
- * @param {object} config Config object
- * @param {string} option Name of option to be checked
- * @param {number} defaultValue Default fallback value
- * @param {number} minBound Optional minimum allowed value
- * @param {number} maxBound Optional maximum allowed value
- * @returns {number} Cleared value
- */
-const checkIntegerOption = (
-  config,
-  option,
-  defaultValue,
-  minBound = undefined,
-  maxBound = undefined,
-) => {
-  const value = checkNumericOption(config, option, defaultValue, minBound, maxBound);
-  if (value !== undefined && !Number.isInteger(value)) {
-    const roundedValue = Math.round(value);
-    log(`Invalid integer option ${option}: [${value}]; rounding value to ${roundedValue}`);
-    return roundedValue;
-  }
-  return value;
-};
+import { checkNumericOption, checkIntegerOption } from './checkOption';
 
 /**
  * Starting from the given index, increment the index until an array element with a
