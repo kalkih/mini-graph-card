9b9a118e5ce34c87ecb1f794283f3c16ab965e91
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-09-07 06:47:59 +0300

    rename "fill_baseline" -> "baseline"

--- src/main.js
+++ src/main.js
@@ -233,9 +233,9 @@ class MiniGraphCard extends LitElement {
           bar_spacing: this.config.bar_spacing,
           bar_spacing_group: this.config.bar_spacing_group,
           total_bars_in_group: this.visibleEntities.length,
-          fill_baseline: getFirstDefinedItem(
-            entity.fill_baseline,
-            this.config.fill_baseline,
+          baseline: getFirstDefinedItem(
+            entity.baseline,
+            this.config.baseline,
           ),
         }),
       );
