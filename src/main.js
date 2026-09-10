d2ae61ef2a602ac1e25680f27b05456cdcd01b25
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-09-10 19:48:10 +0300

    linter

--- src/main.js
+++ src/main.js
@@ -218,13 +218,13 @@ class MiniGraphCard extends LitElement {
     });
 
     // array of "smoothing" values for each graph
-    this._graphSmoothing = this.config.entities.map((entityConfig, index) => this._isBarGraph[index]
+    this._graphSmoothing = this.config.entities.map((entityConfig, index) => (this._isBarGraph[index]
       ? false
       : getFirstDefinedItem(
           entityConfig.smoothing,
           this.config.smoothing,
           this.getDefaultSmoothing(index),
-        )
+        ))
     );
 
     this._md5Config = SparkMD5.hash(JSON.stringify(this.config));
