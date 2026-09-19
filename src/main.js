a7c196b39c26fec5501c3a6b863b0a27925ee937
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-09-20 02:16:39 +0300

    linter

--- src/main.js
+++ src/main.js
@@ -119,6 +119,7 @@ class MiniGraphCard extends LitElement {
       // log invalid entity_ids (once per error to prevent spam)
       if (hass && entityConfig.entity) {
         const errorIndex = this._loggedEntityErrors.indexOf(entityConfig.entity);
+        // eslint-disable-next-line no-lonely-if
         if (!stateObj) {
           // invalid entity
           // check if the error was already handled
