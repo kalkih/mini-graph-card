5ce2c2038b42abaf4f68230065cdd4e24fa0b4f3
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-09-21 08:59:15 +0300

    simplify a bit

--- src/graph.js
+++ src/graph.js
@@ -431,10 +431,10 @@ export default class Graph {
 
   _median(items) {
     const itemsDup = [...items].sort((a, b) => parseFloat(a.state) - parseFloat(b.state));
-    const mid = Math.floor((itemsDup.length - 1) / 2);
+    const mid = Math.floor(itemsDup.length / 2);
     if (itemsDup.length % 2 === 1)
       return parseFloat(itemsDup[mid].state);
-    return (parseFloat(itemsDup[mid].state) + parseFloat(itemsDup[mid + 1].state)) / 2;
+    return (parseFloat(itemsDup[mid - 1].state) + parseFloat(itemsDup[mid].state)) / 2;
   }
 
   _maximum(items) {
